506288a0f78c77ad19cde89b9b0883de903c7aca
lhjd <hongjinlau@gmail.com>
2019-10-23 23:37:11 +0800

    Add layout and bootstrap

--- views/Layout/DefaultLayout.jsx
+++ views/Layout/DefaultLayout.jsx
@@ -5,11 +5,38 @@ class DefaultLayout extends React.Component {
     return (
       <html>
         <head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
           <title>{this.props.title}</title>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"
+            integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc="
+            crossorigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css"
+          />
+          {/* <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous" /> */}
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossorigin="anonymous"
+          />
+          <link rel="stylesheet" href="/style.css"/>
         </head>
         <body>
-        {this.props.children}
-        <script src="/script.js"></script>
+          {this.props.children}
+          <script src="/script.js"></script>
+          {/* <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+        <script dangerouslySetInnerHTML={{__html: `$(document).ready(function() { $('body').bootstrapMaterialDesign(); });`}} /> */}
         </body>
       </html>
     );
--- views/UI/Footer.jsx
+++ views/UI/Footer.jsx
@@ -3,25 +3,48 @@ const React = require("react");
 class Footer extends React.Component {
   render() {
     return (
-      <footer>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/appointment">Appointment</a>
-          </li>
-          <li>
-            <a href="/weight">Weight</a>
-          </li>
-          <li>
-            <a href="/allergy">Allergy</a>
-          </li>
-          <li>
-            <a href="/kibbles">Kibbles</a>
-          </li>
-        </ul>
-      </footer>
+      <nav class="navbar fixed-bottom navbar-dark bg-dark">
+        <a class="navbar-brand text-center" href="/">
+          <i class="fas fa-home"></i>
+          <h6 class="icon-text">Home</h6>
+        </a>
+        <a class="navbar-brand text-center" href="/appointment">
+          <i class="fas fa-calendar-alt"></i>
+          <h6 class="icon-text">Appointment</h6>
+        </a>
+        <a class="navbar-brand text-center" href="/weight">
+          <i class="fas fa-weight"></i>
+          <h6 class="icon-text">Weight</h6>
+        </a>
+        <a class="navbar-brand text-center" href="/kibbles">
+          <i class="fas fa-ring"></i>
+          <h6 class="icon-text">Kibbles</h6>
+        </a>
+        <a class="navbar-brand text-center" href="/allergy">
+          <i class="fas fa-bomb"></i>
+          <h6 class="icon-text">Allergy</h6>
+        </a>
+      </nav>
+
+      // <footer>
+      //   <ul>
+      //     <li>
+      //       <a href="/">Home</a>
+      //     </li>
+      //     <li>
+      //       <a href="/appointment">Appointment</a>
+      //     </li>
+      //     <li>
+      //       <a href="/weight">Weight</a>
+      //     </li>
+      //     <li>
+      //       <a href="/allergy">Allergy</a>
+      //     </li>
+      //     <li>
+      //       <a href="/kibbles">Kibbles</a>
+      //     </li>
+      //   </ul>
+      // </footer>
     );
   }
 }
