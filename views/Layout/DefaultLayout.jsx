620d8b1cf7efc6106b379ea93b276f753ef663c2
lhjd <hongjinlau@gmail.com>
2019-10-29 15:27:16 +0800

    Add manifest

--- views/Layout/DefaultLayout.jsx
+++ views/Layout/DefaultLayout.jsx
@@ -8,6 +8,8 @@ class DefaultLayout extends React.Component {
       <html>
         <head>
           <meta charSet="utf-8" />
+          <meta name="theme-color" content="#494ca2" />
+          <link rel="manifest" href="/manifest.json" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
